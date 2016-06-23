56d5ebefad504298d1f6e1201b6ec050325ff740
kkurinny <kkurinny@thumbtack.net>
2016-06-23 18:52:38 +0600

    reformat unit tests

--- ConsoleTest/Program.cs
+++ ConsoleTest/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using JsonParse;
 
 namespace ConsoleTest
@@ -9,7 +10,13 @@ namespace ConsoleTest
 
         static void Main(string[] args)
         {
-           JsonParser jsonParser = new JsonParser();
+            StringBuilder str = new StringBuilder("sdfsdf");
+            var contains = str.Append("sdfsdf");
+            int[] sdf = new[] {1, 3, 4, 5, 6};
+            var length = sdf.Length;
+            Console.WriteLine("sdfsdf");
+
+            JsonParser jsonParser = new JsonParser();
             string inputString = "{\"UserID\":53568,\"FirstName\":\"Kat\",\"LastName\":\"Burke\",\"City\":{\"Count\":2342234,\"Name\":\"Omsk\",\"Country\":{\"Name\":\"Russia\"}}}";
 
             var handleString = jsonParser.HandleString(inputString);
---
+++ JsonParseTest/JsonParserTest.cs
@@ -0,0 +1,86 @@
+﻿using FluentAssertions;
+using JsonParse;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace JsonParseTest
+{
+    [TestClass]
+    public class JsonParserTest
+    {
+
+        [TestMethod]
+        public void SmallSet()
+        {
+            //init
+            string inputString = "{\"UserID\":53568,\"FirstName\":\"Kat\",\"LastName\":\"Burke\"}";
+            JsonParser jsonParser = new JsonParser();
+            
+            //action
+            string outputString = jsonParser.HandleString(inputString);
+            
+            //assert
+            outputString.Should().Be("{\n  \"UserID\":53568,\n  \"FirstName\":\"Kat\",\n  \"LastName\":\"Burke\"\n}");
+        }
+
+        [TestMethod]
+        public void EndResultWithInsertedSet()
+        {
+            //init
+            string inputString = "{\"UserID\":53568,\"FirstName\":\"Kat\",\"LastName\":\"Burke\",\"City\":{\"Count\":2342234,\"Name\":\"Omsk\",\"Country\":{\"Name\":\"Russia\"}}}";
+            JsonParser jsonParser = new JsonParser();
+     
+            //action
+            string outputString = jsonParser.HandleString(inputString);
+            
+            //assert
+            outputString.Should()
+                .Be(
+                    "{\n  \"UserID\":53568,\n  \"FirstName\":\"Kat\",\n  \"LastName\":\"Burke\",\n  \"City\":\n  {\n    \"Count\":2342234,\n    \"Name\":\"Omsk\",\n    \"Country\":\n    {\n      \"Name\":\"Russia\"\n    }\n  }\n}");
+        }
+
+        [TestMethod]
+        public void SetTabConst_HandleStringWith2Tabs()
+        {
+            //init
+            string inputString = "{\"UserID\":53568,\"FirstName\":\"Kat\",\"LastName\":\"Burke\"}";
+            JsonParser jsonParser = new JsonParser();
+            jsonParser.SetTabConst(2);
+        
+            //action
+            var result = jsonParser.HandleString(inputString);
+            
+            //assert
+            result.Should().Be("{\n  \"UserID\":53568,\n  \"FirstName\":\"Kat\",\n  \"LastName\":\"Burke\"\n}");
+        }
+
+        [TestMethod]
+        public void SetTabConst_HandleStringWith5Tabs()
+        {
+            //init
+            string inputString = "{\"UserID\":53568,\"FirstName\":\"Kat\",\"LastName\":\"Burke\"}";
+            JsonParser jsonParser = new JsonParser();
+            jsonParser.SetTabConst(5);
+        
+            //action
+            var result = jsonParser.HandleString(inputString);
+            
+            //assert
+            result.Should().Be("{\n     \"UserID\":53568,\n     \"FirstName\":\"Kat\",\n     \"LastName\":\"Burke\"\n}");
+        }
+
+        [TestMethod]
+        public void SetLineBreakeConst_HandleStringWith2LineBreake()
+        {
+            //init
+            string inputString = "{\"UserID\":53568,\"FirstName\":\"Kat\",\"LastName\":\"Burke\"}";
+            JsonParser jsonParser = new JsonParser();
+            jsonParser.SetLineBreakeConst(2);
+     
+            //action
+            var result = jsonParser.HandleString(inputString);
+           
+            //assert
+            result.Should().Be("{\n\n  \"UserID\":53568,\n\n  \"FirstName\":\"Kat\",\n\n  \"LastName\":\"Burke\"\n\n}");
+        }
+    }
+}
